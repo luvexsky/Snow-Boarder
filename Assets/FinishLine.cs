af8683a61174de898dcf925b6e5a1b0161cd980a
luvexsky <ngryemo@gmail.com>
2024-09-29 21:25:37 -0400

    Head hitting

--- Assets/FinishLine.cs
+++ Assets/FinishLine.cs
@@ -6,7 +6,10 @@ public class FinishLine : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
+        if(other.tag == "Player")
+        {
         Debug.Log("You finished!");
+        }
     }
     
 }
